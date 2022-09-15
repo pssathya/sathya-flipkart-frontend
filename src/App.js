f81b85d46e25742ac53811f8e5b4dec618736a25
ps_sathya@yahoo.com <ps_sathya@yahoo.com>
2022-09-15 21:05:29 +0530

    [updating App.js]

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from './components/header/Header';
 import Home from './components/home/Home';
 import DetailView from './components/details/DetailView';
 import ContextProvider from './context/ContextProvider';
+import Cart from './components/cart/Cart';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Box style={{marginTop: 54}}>
             <Routes>
               <Route path= '/' element={<Home />} />
+              <Route path= '/cart' element={<Cart />} />
               <Route path= '/product/:id' element={<DetailView />} />
             </Routes>
           </Box>
