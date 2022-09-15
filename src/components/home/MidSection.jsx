caf922f09d83fbeb02e5941e36aa17c5da8340ca
ps_sathya@yahoo.com <ps_sathya@yahoo.com>
2022-09-15 20:17:41 +0530

    [Moving image urls from MidSection.jsx to constants/data.js]

--- src/components/home/MidSection.jsx
+++ src/components/home/MidSection.jsx
@@ -1,10 +1,5 @@
 import {  Grid, styled } from '@mui/material';
-
-const ImageURL = [
-    'https://rukminim1.flixcart.com/flap/960/960/image/2f30db9425df5cec.jpg?q=50',
-    'https://rukminim1.flixcart.com/flap/960/960/image/084789479074d2b2.jpg',
-    'https://rukminim1.flixcart.com/flap/960/960/image/1ce0c4c1fb501b45.jpg?q=50'
-];
+import { imageURL } from "../../constants/data";
 
 const Wrapper = styled(Grid)`
     display: flex;
@@ -29,7 +24,7 @@ const MidSection = () => {
         <>
             <Wrapper lg={12} sm={12} md={12} xs={12} container>
                 {
-                    ImageURL.map(image => (
+                    imageURL.map(image => (
                         <Grid item lg={4} md={4} sm={12} xs={12}>
                             <img src={image} alt="ad" style={{ width: '100%' }} />
                         </Grid>
