32fe07c65a06261be7611278e055771f32997f60
ps_sathya@yahoo.com <ps_sathya@yahoo.com>
2022-09-15 18:33:56 +0530

    [Adding context/ContextProvider.jsx and its references]

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import './App.css';
 
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import DataProvider from './context/ContextProvider';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Box style={{ marginTop:54 }}>
-        <Home/>
-      </Box>
+      <DataProvider>
+        <Header />
+        <Box style={{ marginTop: 54 }}>
+          <Home />
+        </Box>
+      </DataProvider>
     </div>
   );
 }
