81572396175b4e3f168b854fd2df363cf7701ffe
ps_sathya@yahoo.com <ps_sathya@yahoo.com>
2022-09-15 12:00:35 +0530

    [Adding Banner.jsx and its references in Home.jsx]

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,22 @@
+import React from 'react';
 
-//
+import { Box, styled } from '@mui/material';
+
 import NavBar from "./NavBar";
+import Banner  from "./Banner";
+
+const Component = styled(Box)`
+    padding: 20px 10px;
+    background: #F2F2F2;
+`;
 
 const Home = ()=> {
     return (
         <>
-        <NavBar/>
+            <NavBar/>
+            <Component>
+                <Banner/>
+            </Component>
         </>
     )
 }
