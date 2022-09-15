4bae85955596072811a93cd6346e0514a7b310e5
ps_sathya@yahoo.com <ps_sathya@yahoo.com>
2022-09-15 20:50:33 +0530

    [Adding product api and paytm api]

--- src/service/api.js
+++ src/service/api.js
@@ -19,3 +19,22 @@ export const authenticateSignup = async (user) => {
         return error.response;
     }
 }
+
+export const getProductById = async (id) => {
+    try {
+        return await axios.get(`${url}/product/${id}`);
+    } catch (error) {
+        console.log('Error while getting product by id response', error);
+        return error.response;
+    }
+}
+
+export  const payUsingPaytm = async (data) => {
+    try {
+        let response = await axios.post(`${url}/payment`, data);
+        return response.data;
+    } catch (error) {
+        console.log('error', error);
+        return error.response;
+    }
+}
